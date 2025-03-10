4966597dff1f8588a1fa3ae546a20e1ef0f21475
dev-emran <emranhossain094@gmail.com>
2025-03-10 08:49:19 +0600

    database connection dynamic

---
+++ Database.php
@@ -0,0 +1,21 @@
+<?php
+//connect database and run query
+
+class Database
+{
+    public $conn;
+    public function __construct($config, $username = 'root', $password = '')
+    {  
+        $dsn        = 'mysql:' . http_build_query($config, '', ';');
+        $this->conn = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]);
+    }
+
+    public function query($query, $params = [])
+    {
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute($params);
+        return $stmt;
+    }
+}
--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
 require "functions.php";
+require 'Database.php';
 
-require 'router/routers.php';
+// require 'router/routers.php';
+$config = require 'config.php';
+
+
+
+$db = new Database($config['database']);
+$id = $_GET['id'];
+$query = "SELECT * FROM posts WHERE id = ?";
+$post = $db->query($query, [$id])->fetch();
+dd($post);
